abf896af8466d97a4eb70312706a250bf1107ab0
Liams98 <visual-studio-code>
2019-05-18 16:53:15 +0300

    step 4 done

---
+++ Exercises/Javascript-Basics-01/Script_4.js
@@ -0,0 +1,9 @@
+var usr_name = prompt("What's your name?");
+var usr_surename = prompt("What's your surname");
+var usr_city = prompt("What city are you from");
+
+alert
+("Hello!!" + "\n" +
+ "Nom : " + usr_name + "\n" +
+ "Prenom : " + usr_surename + "\n" +
+ "Ville : " + usr_city)
