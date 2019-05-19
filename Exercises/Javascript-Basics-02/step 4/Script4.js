dc0ec0fe92a2783f02c2311a6e007d97d0ef62a6
Liams98 <visual-studio-code>
2019-05-19 16:29:44 +0300

    step4 JS_B2

---
+++ Exercises/Javascript-Basics-02/step 4/Script4.js
@@ -0,0 +1,7 @@
+var reset_btn = document.querySelector("button");
+
+reset_btn.onclick =()=>{
+    document.querySelector("#name").value = "";
+    document.querySelector("#surname").value = "";
+    document.querySelector("#city").value = "";
+}
