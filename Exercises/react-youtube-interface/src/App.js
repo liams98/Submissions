28513fb022d32fd24dc8379539fd803b61968e94
Liams98 <visual-studio-code>
2019-05-29 09:26:15 +0300

    sharing code with kareem

--- Exercises/react-youtube-interface/src/App.js
+++ Exercises/react-youtube-interface/src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
+import MainVid from "./MainVid";
+import { generateKeyPair } from "crypto";
+import { async } from "q";
+
 function App() {
+
+ const appkey = "AIzaSyDYp8XFqoyeJhqqdjKXyVP3-rh5kJibA_4";
+  
+ 
+  
+
+//  const getReq = async()=>{
+//    const response = await fetch(`https://www.googleapis.com/youtube/v3/search`);
+//     const data = response.json();
+//     console.log("data => ",data);
+//  }
+
+ fetch(`https://www.googleapis.com/youtube/v3/search`)
+ .then(response =>{
+   response.json();
+   console.log(response.json)
+ })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // MAIN PAGE
+    <div className = "MainApp">
+      <div className = "top">
+
+        <div className = "nav-left">
+          <img className = "YT-logo" src="http://ubs.production.na1.netsuitestaging.com/site/images/logos/yt_logo_rgb_light.png" alt="" width="100px" height = "35px"/>
+        </div>
+
+        <form className = "search-form">
+          <input className = "search-bar" type="text" placeholder = "Search"/>
+          <button className = "search-button">Search</button>
+        </form>
+
+        <div className = "nav-right">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+        </div>
+      </div>
+
+      <div className = "columns">
+        {/* Main Video */}
+        <div className = "mainVid">
+          <MainVid />
+        </div>
+
+
+        {/* list of videos on the right */}
+        <div className = "listVid">
+          {
+
+          }
+        </div>
+      </div>
+      
     </div>
   );
 }
---
+++ Exercises/react-youtube-interface/src/MainVid.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"
+import { directive } from "@babel/types";
+
+function MainVid(vid_url, title){
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default MainVid;
