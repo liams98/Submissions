f29050dee307246d7973b7d57c0f873c933e21bd
Liams98 <visual-studio-code>
2019-06-11 12:53:55 +0300

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -4,5 +4,14 @@ var app = express()
 app.get('/',function(req, res, next){
     res.send('<h1>ok</h1>')
 })
+app.get('/test',function(req,res){
+    res.send({'status':"200", "message":"ok"})
+})
 
+app.get('/time', function(req,res){
+    res.send({'status':"200", "message": time})
+})
 app.listen(3000);
+
+var today = new Date();
+var time = today.getHours() + ":" + today.getSeconds();
