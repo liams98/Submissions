ab5b5d088f9e248b4ed897c7800856d9336949c4
Liams98 <visual-studio-code>
2019-06-12 12:36:34 +0300

    step 7

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -24,56 +24,81 @@ app.get(`/hello/:tagId?`, function(req, res) {
   res.send({ status: "200", message: `hello,` + check() });
 });
 
-app.get("/search", function(req,res){
-  var s = req.query.s
-  if(s !== ""){
-  res.send({status:200, message:"ok", data: s})
-  }else{
-    res.send({status:"500",error:"true",message:"you have to provide a search"})
-  }
-})
-
-const movies = [
-  { title: 'Jaws', year: 1975, rating: 8 },
-  { title: 'Avatar', year: 2009, rating: 7.8 },
-  { title: 'Brazil', year: 1985, rating: 8 },
-  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
-]
-
-//CRUD ROUTES---------------
-
-app.get("/movies/create", function(req,res){
- 
-})
-
-app.get("/movies/read/:BYDATE?", function(req, res) {
-  listbydate = req.params.BYDATE;
-
-
-
-  if (listbydate == "by-date") {
-    res.send({ status: "200", data: movies.sort(function(a,b){return a.year - b.year})});
-  }else if(listbydate == "by-rating"){
-    res.send({status:200, data: movies.sort(function(a,b){return a.rating - b.rating})})
-  }else if(listbydate == "by-title"){
-    res.send({status:200, data: movies.sort(function(a,b){
-      var x = a.title;
-      var y = b.title;
-      if (x < y) {return -1;}
-      if (x > y) {return 1;}
-      return 0;})})
+app.get("/search", function(req, res) {
+  var s = req.query.s;
+  if (s !== "") {
+    res.send({ status: 200, message: "ok", data: s });
+  } else {
+    res.send({
+      status: "500",
+      error: "true",
+      message: "you have to provide a search"
+    });
   }
 });
 
-app.get("/movies/update", function(req,res){
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 }
+];
+
+//CRUD ROUTES---------------
+
+app.get("/movies/create", function(req, res) {});
+
+app.get("/movies/read/:TAGID?/:ID?", function(req, res) {
+  TAG = req.params.TAGID;
+  id = req.params.ID
   
-})
-
-app.get("/movies/delete", function(req,res){
+  if (TAG == "by-date") {
+    res.send({
+      status: "200",
+      data: movies.sort(function(a, b) {
+        return a.year - b.year;
+      })
+    });
+  }
   
-})
+  else if (TAG == "by-rating") {
+    res.send({
+      status: 200,
+      data: movies.sort(function(a, b) {
+        return a.rating - b.rating;
+      })
+    });
+  }
 
+   else if (TAG == "by-title") {
+    res.send({
+      status: 200,
+      data: movies.sort(function(a, b) {
+        var x = a.title;
+        var y = b.title;
+        if (x < y) {
+          return -1;
+        }
+        if (x > y) {
+          return 1;
+        }
+        return 0;
+      })
+    });
+  }
 
+  if(TAG == "id" && id < movies.length){
+    res.send(movies[id])
+  }else{
+    res.status(404).send({status:404, error:true, message:`the movie ${id} does not exist`})
+  }
+  
+
+});
+
+app.get("/movies/update", function(req, res) {});
+
+app.get("/movies/delete", function(req, res) {});
 
 app.listen(3000);
 
