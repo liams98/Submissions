11b8d9485f9b2558680f6978d1553758692e9bc0
Liams98 <visual-studio-code>
2019-06-12 10:26:28 +0300

    step 5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -9,7 +9,6 @@ app.get("/test", function(req, res) {
 });
 
 app.get("/time", function(req, res) {
-  console.log(req.query)
   res.send({ status: "200", message: time });
 });
 
@@ -33,6 +32,33 @@ app.get("/search", function(req,res){
     res.send({status:"500",error:"true",message:"you have to provide a search"})
   }
 })
+
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+//CRUD ROUTES---------------
+
+app.get("/movies/create", function(req,res){
+ 
+})
+
+app.get("/movies/read", function(req,res){
+  res.send({status:"200", data: movies})
+})
+
+app.get("/movies/update", function(req,res){
+  
+})
+
+app.get("/movies/delete", function(req,res){
+  
+})
+
+
 
 app.listen(3000);
 
